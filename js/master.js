b3441e888d044a8bf6f04c703694e719ac162d7f
Jose Moreno <jose.moreno@reyesic.net>
2014-12-25 18:24:01 +0000

    Gitignore update

--- js/master.js
+++ js/master.js
@@ -1,8 +1,10 @@
 angular.module('ngMadLibs', []).controller('MadLibs',function($scope){
-    $scope.data = {person_name: '(Name)', job_title: '(Job)', tedious_task: '(Tedious Task)', dirty_task: '(Dirty Task)', celebrity: '(Celebrity)', useless_skill: '(Useless Skill)', adjective: '(Adjective)', obnoxiuous_celebrity: '(Obnoxiuous Celebrity)', huge_number: '(Huge Number)'};
+    $scope.data = {person_name: '', job_title: '', tedious_task: '', dirty_task: '', celebrity: '', useless_skill: '', adjective: '', obnoxiuous_celebrity: '', huge_number: ''};
     $scope.gender = {pos: 'her', pron: 'she', obj: 'her'};
     $scope.boy = {pos: 'his', pron: 'he', obj: 'him'};
     $scope.girl = {pos: 'her', pron: 'she', obj: 'her' };
+    $scope.user_input = {person_name: 'name', job_title: 'job title', tedious_task: 'tedious task', dirty_task: 'task', celebrity: 'name', useless_skill: 'skill', adjective: 'adjective', obnoxiuous_celebrity: 'name', huge_number: 'number'};
+    
     
     $scope.changeGender = function(gender) {
         $scope.gender.pos = gender.pos;
