9e98a644d00ef1ce38a118a4d34f56354ddcb5a7
Jose Moreno <jose.moreno@reyesic.net>
2015-01-02 01:49:32 +0000

    Add animations

--- js/master.js
+++ js/master.js
@@ -1,11 +1,11 @@
-angular.module('ngMadLibs', []).controller('MadLibs',function($scope){
+angular.module('ngMadLibs', ['ngAnimate']).controller('MadLibs', function($scope){
     $scope.data = {person_name: '', job_title: '', tedious_task: '', dirty_task: '', celebrity: '', useless_skill: '', adjective: '', obnoxiuous_celebrity: '', huge_number: ''};
     $scope.init = $scope.data;
     $scope.gender = {pos: 'her', pron: 'she', obj: 'her'};
     $scope.boy = {pos: 'his', pron: 'he', obj: 'him'};
     $scope.girl = {pos: 'her', pron: 'she', obj: 'her' };
-    $scope.reset = function(app){
-    	app.$submitted = false;
+    $scope.reset = function(){
+        $scope.changeStatus();
     	$scope.data = {person_name: '', job_title: '', tedious_task: '', dirty_task: '', celebrity: '', useless_skill: '', adjective: '', obnoxiuous_celebrity: '', huge_number: ''};
     };
     
@@ -15,4 +15,11 @@ angular.module('ngMadLibs', []).controller('MadLibs',function($scope){
         $scope.gender.obj = gender.obj;
     };
     
+    $scope.changeStatus = function() {
+            $scope.showInput = !$scope.showInput;
+            $scope.showText = !$scope.showText;
+            console.log("works!");
+            console.log($scope.showText);
+            console.log($scope.showInput);
+    };
 });
